9279d028c0b316e539265937b2c039418d718e17
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2023-11-28 23:33:31 -0300

    add get_dataset

---
+++ code/get_dataset.R
@@ -0,0 +1,23 @@
+library(readr)
+
+## Get the dataframe
+get_dataframe <- function(file_path) {
+    dataframe <-  readr::read_csv(file_path)
+    return(dataframe)
+}
+
+## Get train dataframe 
+get_train_df <- function() {
+    file_path <- "dataset/fake_accounts_train.csv"
+    dataframe <- get_dataframe(file_path)
+    return(dataframe)
+}
+
+## Get test dataframe
+get_test_df <- function() {
+    file_path <- "dataset/fake_accounts_test.csv"
+    dataframe <- get_dataframe(file_path)
+    return(dataframe)
+}
+
+train <- get_train_df()
