41a5c423b89193a5d43665a84d72f32fd2d0afb9
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2024-07-21 16:08:44 -0300

    add remove_ni_lt_cz results

--- code/main.R
+++ code/main.R
@@ -14,11 +14,11 @@ train_variables <- list(
     ntree = 100,
     mtry = 5,
     scale_data = FALSE,
-    remove_non_image_post_percentage = FALSE,
-    remove_location_tag_percentage = FALSE,
+    remove_non_image_post_percentage = TRUE,
+    remove_location_tag_percentage = TRUE,
     remove_comments_engagement_rate = FALSE,
-    remove_caption_zero = FALSE, 
-    add_follow_difference = TRUE,
+    remove_caption_zero = TRUE, 
+    add_follow_difference = FALSE,
     add_follow_rate = FALSE,
     add_account_age = FALSE,
     add_follower_frequency = FALSE,
@@ -28,7 +28,7 @@ train_variables <- list(
 
 
 ## Set file name of the output md
-file_name <- "follow_difference"
+file_name <- "remove_ni_lt_cz"
 
 ## train the model
 model <- train_model(train_variables)
